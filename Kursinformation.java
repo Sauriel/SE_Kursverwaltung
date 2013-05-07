b2a697a60f98577d66674f0473d657a679dc5259
Sarchibal <darrk-link@web.de>
2013-05-07 14:52:30 +0200

    Hab die main rausgenommen.

--- Kursinformation.java
+++ Kursinformation.java
@@ -25,9 +25,4 @@ public class Kursinformation {
 	public String toString() {
 		return ("Daten: " + daten + "\nAngebot: " + angebot + "\nVoraussetzungen: " + voraussetzungen);
 	}
-	
-	public static void main(String[] args) {
-		Kursinformation k = new Kursinformation("a", "b", "c");
-		System.out.println(k.toString());
-	}
 }
