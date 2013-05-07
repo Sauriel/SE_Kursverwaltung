ae977c3507d9ce8ba5a44b722d48ccfb7be3758c
Sascha Drux <sascha.drux@haw-hamburg.de>
2013-05-07 14:22:17 +0200

    Student Konstruktor + toString

--- Student.java
+++ Student.java
@@ -1,4 +1,17 @@
 public class Student {
 
+	private String vorname;
+	private String name;
+	private int matrikelnummer;
+	
 
+	public Student(String vorname, String name, int matrikelnummer) {
+		this.vorname = vorname;
+		this.name = name;
+		this.matrikelnummer = matrikelnummer
+	}
+	
+	public String toString() {
+		return "Name: " + vorname + " " + name + ", Matrikelnummer: " + matrikelnummer;
+	}
 }
